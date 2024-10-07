a6e6863a1a2030284dadcce9c2f1e1e4ea599b5a
Daniel <d-wintermeyer@web.de>
2024-10-07 21:43:53 +0200

    Feat: Minor fixes for prod build

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,15 +4,15 @@ import type { MetadataRoute } from "next";
 export const baseUrl = "https://revboost-webapp-avyu.vercel.app";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.metadata.publishedAt,
-  }));
+  // const blogs = getBlogPosts().map((post) => ({
+  //   url: `${baseUrl}/blog/${post.slug}`,
+  //   lastModified: post.metadata.publishedAt,
+  // }));
 
   const routes = ["", "/blog"].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  return [...routes, ...blogs];
+  return [...routes];
 }
