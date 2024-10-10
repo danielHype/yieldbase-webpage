eefca9f43eee0e417e1738a649ebb9ac37a5fa38
Daniel <d-wintermeyer@web.de>
2024-10-10 22:33:10 +0200

    Feat: Change link for webapp

--- src/app/[lng]/signup/page.tsx
+++ src/app/[lng]/signup/page.tsx
@@ -5,13 +5,13 @@ import React, { useEffect } from 'react';
 
 const RedirectPage = () => {
     useEffect(() => {
-        window.location.href = 'https://app.yieldbase.com';
+        window.location.href = 'https://yieldbase-app.vercel.app/';
     }, []);
 
     return (
         <div>
             <h1>Redirecting...</h1>
-            <p>If you are not redirected automatically, follow this <Link href="https://app.google.com">link</Link>.</p>
+            <p>If you are not redirected automatically, follow this <Link href="https://yieldbase-app.vercel.app/">link</Link>.</p>
         </div>
     );
 };
